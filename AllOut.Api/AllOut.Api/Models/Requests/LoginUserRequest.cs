e5c22fd86fd126f6340be8131c57386e1f04cb4e
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-01-15 08:37:16 +0800

    [-] Remove RequestBase in LoginUserRequest.cs

--- AllOut.Api/AllOut.Api/Models/Requests/LoginUserRequest.cs
+++ AllOut.Api/AllOut.Api/Models/Requests/LoginUserRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace AllOut.Api.Models.Requests
 {
-    public class LoginUserRequest : RequestBase
+    public class LoginUserRequest
     {
         public string LogonName { get; set; }
         public string Password { get; set; }
