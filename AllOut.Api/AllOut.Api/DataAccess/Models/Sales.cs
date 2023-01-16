98133c789012e461a3b11d1ddcb5481d8eb0c6c8
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-01-16 12:30:48 +0800

    [+] Add AmountPaid & Change in Sales.cs

--- AllOut.Api/AllOut.Api/DataAccess/Models/Sales.cs
+++ AllOut.Api/AllOut.Api/DataAccess/Models/Sales.cs
@@ -7,6 +7,8 @@ namespace AllOut.Api.DataAccess.Models
         [Key, MaxLength(15)]
         public string SalesID { get; set; }
         public Guid UserID { get; set; }
+        public decimal AmountPaid { get; set; }
+        public decimal Change { get; set; }
         [MaxLength(100)]
         public string? Remarks { get; set; }
         public int Status { get; set; }
