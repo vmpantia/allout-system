4482ac403cd2d5295eebe139e0f01291d096e00c
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-01-16 12:24:16 +0800

    [u] Changes in ProductFullInformation.cs

--- AllOut.Api/AllOut.Api/Models/ProductFullInformation.cs
+++ AllOut.Api/AllOut.Api/Models/ProductFullInformation.cs
@@ -7,7 +7,7 @@ namespace AllOut.Api.Models
     {
         public Guid ProductID { get; set; }
         public string ProductName { get; set; }
-        public string ProductDescription { get; set; }
+        public string? ProductDescription { get; set; }
         public int ReorderPoint { get; set; }
         public decimal Price { get; set; }
         public int Status { get; set; }
