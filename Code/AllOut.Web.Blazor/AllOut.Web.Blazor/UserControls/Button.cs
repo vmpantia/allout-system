e1ecc210cad90e9999b7171510ea1e37c7563cfe
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-02-16 19:59:14 +0800

    [u] Buttons Changes

---
+++ Code/AllOut.Web.Blazor/AllOut.Web.Blazor/UserControls/Button.cs
@@ -0,0 +1,9 @@
+﻿namespace AllOut.Web.Blazor.UserControls
+{
+    public class Button
+    {
+        public string Icon { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+        public string Page { get; set; } = string.Empty;
+    }
+}
