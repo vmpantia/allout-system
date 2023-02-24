08f223f6b1c178a1f217b36578a878fac21655b0
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-02-24 09:45:08 +0800

    [+] Add Category Pages

--- Code/AllOut.Web.Blazor/AllOut.Web.Blazor/Models/Category.cs
+++ Code/AllOut.Web.Blazor/AllOut.Web.Blazor/Models/Category.cs
@@ -2,7 +2,7 @@
 
 namespace AllOut.Web.Blazor.Models
 { 
-    public class Category
+    public class Category : Selection
     {
         public Guid CategoryID { get; set; }
         public string Name { get; set; }
