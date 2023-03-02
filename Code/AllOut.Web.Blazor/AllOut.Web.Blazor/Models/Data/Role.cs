8861db57947446c2a8ef0616b005e2cfceb308ac
Vincent Pantia <vincent.m.pantia@hotmail.com>
2023-03-02 19:18:25 +0800

    [+] Add RoleListComponents

--- Code/AllOut.Web.Blazor/AllOut.Web.Blazor/Models/Data/Role.cs
+++ Code/AllOut.Web.Blazor/AllOut.Web.Blazor/Models/Data/Role.cs
@@ -2,7 +2,7 @@
 
 namespace AllOut.Web.Blazor.Models
 {
-    public class Role
+    public class Role : Selection
     {
         public Guid RoleID { get; set; }
         public string Name { get; set; }
---
+++ Code/AllOut.Web.Blazor/AllOut.Web.Blazor/Models/FullInformation/RoleFullInformation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AllOut.Web.Blazor.Models
+{
+    public class RoleFullInformation : Role
+    {
+        public string Add { get; set; }
+        public string Edit { get; set; }
+        public string Delete { get; set; }
+    }
+}
